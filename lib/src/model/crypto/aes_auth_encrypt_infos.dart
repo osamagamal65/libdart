d4406321138f0225d696b5b12b6da9b9e2a68548
Osama gamal <ogamalii65@gmail.com>
2021-12-17 01:31:50 +0200

    arc

--- lib/src/model/crypto/aes_auth_encrypt_infos.dart
+++ lib/src/model/crypto/aes_auth_encrypt_infos.dart
@@ -3,10 +3,10 @@ import 'dart:typed_data' show Uint8List;
 
 class AesAuthEncryptInfos {
   AesAuthEncryptInfos({
-    required this.tag,
+    this.tag,
     required this.encrypted,
   });
 
-  Uint8List tag;
+  Uint8List? tag;
   Uint8List encrypted;
 }
